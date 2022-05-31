235708d0cfffc5377f7fbd9545cffe85c8b49d51
kimamovic21 <imamovic.kerim@gmail.com>
2022-05-31 16:28:00 +0200

    izmjene
    21:20 video

--- brocode.js
+++
@@ -1,1 +0,0 @@
-const kerim = 'kerim'
---
+++ index.js
@@ -0,0 +1,158 @@
+// #4 User input
+
+// How to accept user input
+// Easy way with a window prompt
+// difficult way HTML textbook
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #3 Arithmetic expressions 
+
+/* arithmetic expression is a combination of... 
+  operands (values, variables, etc...)
+  operators (+ - * / % )
+  that can be evaluated to a value
+  ex. y = x + 5;
+*/
+
+// let students = 20;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+
+// let extraStudents = students % 3; //remainder
+
+// newStudents = students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+
+// console.log(students);
+// console.log(newStudents);
+// console.log(extraStudents);
+
+/* 
+    operator precendence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & divison
+    4. additon & subtraction
+*/
+
+// let result = 1 + 2 * (1+2)**2;
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #2 Variables
+
+// A variable is a container for storing data
+// A varibale behaves as if it was the value that it contains
+
+//Two steps:
+// 1. Declaration (var, let, const)
+// 2. Assignment (assignment operator)
+// Descriptive variable
+
+// strings, numbers and booleans
+
+//let age; 
+//age = 25;
+// let firstName = "Kerim"; // strings
+
+// let age = 25; // numbers
+//age = age + 1;
+
+// let student = false; // boolean
+
+
+// console.log(firstName);
+// console.log(age);
+// console.log(student);
+
+// console.log("Hello", firstName);
+// console.log("You are", age);
+// console.log("Enrolles:", student);
+// Ako ne dodamo nikakvu vrijednost onda ce biti nedefinisano
+
+// DOM
+// document.getElementById("p1").innerHTML = "Hello " + firstName;
+// document.getElementById("p2").innerHTML = "You are " + age + "years old";
+// document.getElementById("p3").innerHTML = "Enrolled: " + student;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #1 Introduction to javascript
+
+//console.log("Kerim");
+//console.log("Imamovic");
+
+//window.alert('My name is Kerim Imamovic');
+
+// This is a comment
+
+/* 
+This 
+is 
+a
+multiline 
+commment 
+*/
