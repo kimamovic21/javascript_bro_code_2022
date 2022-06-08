72e35b7c50dca79a81f37ca99ad09a75034e99d1
kimamovic21 <imamovic.kerim@gmail.com>
2022-06-08 18:34:44 +0200

    izmjene
    2:15:19 video

--- index.js
+++ index.js
@@ -1,3 +1,112 @@
+// #31 temperature conversion program
+
+
+// 2. primjer
+
+// document.getElementById("submitButton").onclick = function() {
+
+//   let temp;
+
+//   if(document.getElementById("cButton").checked) {
+//     temp = document.getElementById("textBox").value;
+//     temp = Number(temp);
+//     temp = toCelsius(temp);
+//     document.getElementById("tempLabel").innerHTML = temp + "C";
+//   } 
+//   else if(document.getElementById("fButton").checked) {
+//     temp = document.getElementById("textBox").value;
+//     temp = Number(temp);
+//     temp = toFahrenheit(temp);
+//     document.getElementById("tempLabel").innerHTML = temp + "F";
+//   } 
+//   else {
+//     document.getElementById("tempLabel").innerHTML = "Select a unit";
+//   }
+
+// }
+ 
+
+// function toCelsius(temp) {
+//   return (temp - 32) * (5/9);
+// }
+
+// function toFahrenheit(temp) {
+//   return temp * 9 / 5 + 32;
+// }
+
+
+
+
+
+// 1. primjer
+
+// let temp = 32;
+// // temp = toCelsius(temp);
+// temp = toFahrenheit(temp);
+// console.log(temp);
+
+// function toCelsius(temp) {
+//   return (temp - 32) * (5/9);
+// }
+
+// function toFahrenheit(temp) {
+//   return temp * 9 / 5 + 32;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #30 number guessing game
+
+// const answer = Math.floor(Math.random() * 10 + 1); // random number between 1 and 10
+// let guesses = 0;
+
+// document.getElementById("submitButton").onclick = function() {
+
+//     let guess = document.getElementById("guessField").value;
+//     guesses+=1;
+
+//     if (guess == answer) {
+//       alert(`${answer} is the number. It took you ${guesses} guesses`);
+//     }
+//     else if (guess < answer) {
+//       alert("Tooo small...");
+//     }
+//     else {
+//       alert("Too large...");
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // #29 toLocaleString()
 
 // returns a string with a language sensitive representation
