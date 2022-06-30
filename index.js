7424d01751ec44dee835d24a4ccdd86d09deace0
kimamovic21 <imamovic.kerim@gmail.com>
2022-06-30 15:52:32 +0200

    izmjene
    snake game

--- index.js
+++ index.js
@@ -1,3 +1,216 @@
+// #87 Snake game
+
+const gameBoard = document.querySelector("#gameBoard");
+const context = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "white";
+const snakeColor = "lightgreen";
+const snakeBorder = "black";
+const foodColor = "red";
+const unitSize = 15;
+
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+  {x:unitSize * 4, y:0},
+  {x:unitSize * 3, y:0},
+  {x:unitSize * 2, y:0},
+  {x:unitSize * 1, y:0},
+  {x:0, y:0}
+]
+
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
+
+gameStart();
+
+// 3. uredujemo gameStart funkciju
+function gameStart(){
+  running = true;
+  scoreText.textContent = score;
+  createFood();
+  drawFood();
+  nextTick();
+};
+
+// 4. uredujemo nextTick funkciju
+function nextTick(){
+  if(running){
+    setTimeout(() => {
+      clearBoard();
+      drawFood();
+      moveSnake();
+      drawSnake();
+      checkGameOver();
+      nextTick();
+    }, 75);
+  }
+  else {
+    displayGameOver();
+  }
+};
+
+// 5. uredujemo ClearBoard funkciju
+function clearBoard(){
+  context.fillStyle = boardBackground;
+  context.fillRect(0, 0, gameWidth, gameHeight);
+};
+
+// 1. uredujemo createFood funkciju
+function createFood(){
+  function randomFood(min, max){
+    const randNum = Math.round((Math.random() * (max - min) + min) / unitSize ) * unitSize;
+    return randNum;
+  }
+  foodX = randomFood(0, gameWidth - unitSize);
+  foodY = randomFood(0, gameWidth - unitSize);
+  // console.log(foodX);
+};
+
+// 2. uredujemo drawFood funkciju
+function drawFood(){
+  context.fillStyle = foodColor;
+  context.fillRect(foodX, foodY, unitSize, unitSize);
+};
+
+// 7. uredujemo moveSnake funkciju
+function moveSnake(){
+  const head = {x: snake[0].x + xVelocity,
+                y: snake[0].y + yVelocity};
+
+  snake.unshift(head);
+  // if food is eaten
+  if(snake[0].x == foodX && snake[0].y == foodY) {
+    score+=1;
+    scoreText.textContent = score;
+    createFood();
+  }
+  else {
+    snake.pop();
+  }
+};
+
+// 6. uredujemo drawSnake funkciju
+function drawSnake(){
+  context.fillStyle = snakeColor;
+  context.strokeStyle = snakeBorder;
+  snake.forEach(snakePart => {
+    context.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
+    context.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
+  });
+};
+
+// 8. uredujemo changeDirection funkciju
+function changeDirection(event){
+  const keyPressed = event.keyCode;
+  const LEFT = 37;
+  const UP = 38;
+  const RIGHT = 39;
+  const DOWN = 40;
+
+  const goingUp = (yVelocity == -unitSize);
+  const goingDown = (yVelocity == unitSize);
+  const goingRight = (xVelocity == unitSize);
+  const goingLeft = (xVelocity == -unitSize);
+
+  switch(true){
+    case(keyPressed == LEFT && !goingRight):
+      xVelocity = -unitSize;
+      yVelocity = 0;
+      break;
+    case(keyPressed == UP && !goingDown):
+      xVelocity = 0;
+      yVelocity = -unitSize;
+      break;
+    case(keyPressed == RIGHT && !goingLeft):
+      xVelocity = unitSize;
+      yVelocity = 0;
+      break;
+    case(keyPressed == DOWN && !goingUp):
+      xVelocity = 0;
+      yVelocity = unitSize;
+      break;
+  }
+};
+
+// 9. uredujemo checkGameOver funkciju
+function checkGameOver(){
+  switch(true){
+    case (snake[0].x < 0):
+      running = false;
+      break;
+    case (snake[0].x >= gameWidth):
+      running = false;
+      break;
+    case(snake[0].y < 0):
+      running = false;
+      break;
+    case(snake[0].y >= gameHeight):
+      running = false;
+      break;
+  }
+  for(let i = 1; i < snake.length; i+=1){
+    if(snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+      running = false;
+    }
+  }
+};
+
+// 10. uredujemo displayGameOver funkciju
+function displayGameOver(){
+  context.font = "50px MV Boli";
+  context.fillStyle = "black";
+  context.textAlign = "center";
+  context.fillText("Game over!", gameWidth / 2, gameHeight / 2);
+  running = false;
+};
+
+// 11. uredujemo resetGame funkciju
+function resetGame(){
+  score = 0;
+  xVelocity = unitSize;
+  yVelocity = 0;
+  snake = [
+    {x:unitSize * 4, y:0},
+    {x:unitSize * 3, y:0},
+    {x:unitSize * 2, y:0},
+    {x:unitSize * 1, y:0},
+    {x:0, y:0}
+  ];
+  gameStart();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // #86 Tic Tac Toe game
 
 // const cells = document.querySelectorAll(".cell");
